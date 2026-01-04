a4530b0742de4841777bbff070a099948876e4ad
Gara Hegarty <Hegarty-G5@ulster.ac.uk>
2026-01-04 19:05:49 +0000

    Deploy test 2

--- src/functions/QueueTriggerUpload.js
+++ src/functions/QueueTriggerUpload.js
@@ -5,6 +5,6 @@ app.storageQueue('QueueTriggerUpload', {
     connection: 'storcom682_STORAGE',
     handler: (queueItem, context) => {
         context.log('Storage queue function processed work item:', queueItem);
-        context.log('DEPLOY TEST - queue item:', queueItem);
+        context.log('DEPLOY TEST - queue item: 2', queueItem);
     }
 });
