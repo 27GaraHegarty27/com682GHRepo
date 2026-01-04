f98672ffda4c60e9467478b4f46d56ecc7862b7f
Gara Hegarty <Hegarty-G5@ulster.ac.uk>
2026-01-04 18:50:22 +0000

    Test deployment2 (remove secrets)

---
+++ src/functions/QueueTriggerUpload.js
@@ -0,0 +1,10 @@
+const { app } = require('@azure/functions');
+
+app.storageQueue('QueueTriggerUpload', {
+    queueName: 'upload-jobs',
+    connection: 'storcom682_STORAGE',
+    handler: (queueItem, context) => {
+        context.log('Storage queue function processed work item:', queueItem);
+        context.log('DEPLOY TEST - queue item:', queueItem);
+    }
+});
